c7946d6725b77f1c5129343eaf89444a9c3bd710
Diogo Santana <diogo.anatnas@gmail.com>
2023-04-09 22:05:46 -0300

    Adicionando ambiente virtual; código python CEP; bibliotecas necessárias.

---
+++ CEP.py
@@ -0,0 +1,25 @@
+import requests
+import pandas as pd
+
+cep = "06429270"
+
+cep = cep.replace("-", "").replace(".", "").replace(" ", "")
+
+if len(cep) == 8:
+    link = f'https://viacep.com.br/ws/{cep}/json/'
+
+    requisicao = requests.get(link)
+
+    print(requisicao)
+
+    dic_requisicao = requisicao.json()
+
+    uf = dic_requisicao['uf']
+    cidade = dic_requisicao['localidade']
+    bairro = dic_requisicao['bairro']
+    print(dic_requisicao)
+else:
+    print("CEP Inválido")
+
+tabela = pd.DataFrame(dic_requisicao)
+print(tabela)
